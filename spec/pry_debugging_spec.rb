7192bca41cfb5603e9b8e7784f539001121ecd4b
Carla Stickler <carlastickler@gmail.com>
2019-03-28 16:27:39 +0000

    Done.

--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -5,5 +5,9 @@ describe "#plus_two" do
 	it "takes in a number as an argument and returns the sum of that number and 2" do
 		expect(plus_two(3)).to eq(5)
 	end
-
+def plus_two(num)
+    sum = num + 2
+    sum 
+    
+end
 end
